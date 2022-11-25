2fa3e173eefc45ce84430320bbc73ad35a5e4a40
Kuong Ao Ieong <majilaiwife@gmail.com>
2022-11-25 12:07:51 +0000

    feat(models, database): inserted the Model schemas
    Implemented the User Schema, have not put in game schema yet as it is hard to see the data right now

---
+++ server/models/index.ts
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const DATABASE_URL = process.env.DATABASE_URL;
+const DATABASE_NAME = process.env.DATABASE_NAME;
+
+mongoose.connect( `${DATABASE_URL}${DATABASE_NAME}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+export {mongoose};
---
+++ server/models/user.ts
@@ -0,0 +1,36 @@
+require("dotenv").config();
+
+import {mongoose} from './index'
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+      },
+      lastName:{
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+      sortingPath:{
+        type:Number,
+        default: 0
+      },
+      pathFindPath: {
+        type:Number,
+        default: 0
+      }
+})
+
+const User = mongoose.model('Users', userSchema)
+
+export {User}
