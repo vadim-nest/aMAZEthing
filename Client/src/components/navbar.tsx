c923b4a7b09ad3d43ceacc1165a787a605697a5b
Marija Vitkauskaite <m.v.vitkauskaite@gmail.com>
2022-11-25 14:15:46 +0000

    feat(navbar.tsx): add useLocation hook

--- Client/src/components/navbar.tsx
+++ Client/src/components/navbar.tsx
@@ -1,15 +1,17 @@
 import '../css/navbar.css';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 
 function Navbar() {
   const navigate = useNavigate();
+  const location = useLocation();
 
   return (
     <div className='navbar'>
       <div className='navbar-start'>
         <p className='maze' onClick={() => navigate('/')}>aMAZEthing</p>
-        <p className='learn' onClick={() => navigate('/learning')}>LEARN</p>
+        {location.pathname ==='/game' && <p className='do' onClick={() => navigate('/learning')}>LEARN</p>}
+        {location.pathname ==='/learning' && <p className='do' onClick={() => navigate('/game')}>PLAY</p>}
       </div>
       <div className='navbar-end'>
         <p className='login' onClick={() => navigate('/login')}>LOGIN</p>
