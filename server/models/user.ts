9a1cc9a531d74f8f3ed5ac966f1618235d552c15
Kuong Ao Ieong <majilaiwife@gmail.com>
2022-11-25 12:23:21 +0000

    feat(models database): added game schema
    added game schema to userSchema

--- server/models/user.ts
+++ server/models/user.ts
@@ -4,6 +4,25 @@ import {mongoose} from './index'
 
 const Schema = mongoose.Schema;
 
+const gameSchema = new Schema({
+    amountMinions:{
+        type:Number
+    },
+    result:{
+        type:String
+    },
+    goldAmount:{
+        type:Number
+    },
+    opponentID:{
+        type: String
+    },
+    towersTaken: {
+        type:Number
+    }
+ })
+
+
 const userSchema = new Schema({
     firstName: {
         type: String,
@@ -28,6 +47,24 @@ const userSchema = new Schema({
       pathFindPath: {
         type:Number,
         default: 0
+      },
+      games:[
+        gameSchema
+      ],
+      overallWins: {
+        wins: {
+            type:Number,
+            default: 0
+        },
+        losses:{
+            type:Number,
+            default: 0
+        },
+        draws:{
+            type:Number,
+            default: 0
+        }
+
       }
 })
 
