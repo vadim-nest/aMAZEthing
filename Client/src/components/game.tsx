3f1077e11a9294085d5e42d481e69381140529f8
Isaac Pillar <isaac.pillar@icloud.com>
2022-11-26 13:58:13 +0000

    feat(game, maze, toolbar, minion): generate minion component, enable clicking of tile and minion
    Game keeps track of which minion and which tile has been clicked most recently

--- Client/src/components/game.tsx
+++ Client/src/components/game.tsx
@@ -3,10 +3,15 @@ import Maze from './maze';
 import GameStats from './gameStats';
 import ToolBar from './toolbar';
 import { useState } from 'react';
+import { minionType } from '../utils/types';
 
 function Game() {
 
-  const [boxSize, setBoxSize] = useState(50);
+  const [boxSize, setBoxSize] = useState(30);
+  const [mazeCompleted, setMazeCompleted] = useState(false);
+  const [minions, setMinions] = useState<minionType[]>([{id: 0, xPos: 1, yPos: 0}, {id: 4, xPos: 2, yPos: 4}]);
+  const [currentMinion, setCurrentMinion] = useState<null | number>(null);
+  const [currentTile, setCurrentTile] = useState<null | {xPos:number, yPos:number}>(null);
   const minBoxSize = 5;
   const maxBoxSize = 100;
 
@@ -15,8 +20,8 @@ function Game() {
       <div>
         <GameStats/>
         <div className='gameContainer'>
-          <ToolBar setBoxSize={setBoxSize} minBoxSize={minBoxSize} maxBoxSize={maxBoxSize}/>
-          <Maze boxSize={boxSize}/>
+          <ToolBar setBoxSize={setBoxSize} minBoxSize={minBoxSize} maxBoxSize={maxBoxSize} currentMinion={currentMinion} currentTile={currentTile}/>
+          <Maze boxSize={boxSize} setMazeCompleted={() => setMazeCompleted(true)} minions={minions} setCurrentMinion={setCurrentMinion} setCurrentTile={setCurrentTile}/>
         </div>
       </div>
     </>
---
+++ Client/src/utils/types.ts
@@ -0,0 +1,5 @@
+export interface minionType {
+  id: number;
+  xPos: number;
+  yPos: number;
+}
