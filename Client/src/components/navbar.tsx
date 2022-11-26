76e1529687a3d5d213a6416ec86df3eeda0ce960
Marija Vitkauskaite <m.v.vitkauskaite@gmail.com>
2022-11-26 11:18:34 +0000

    feat: colors as variables

--- Client/src/components/navbar.tsx
+++ Client/src/components/navbar.tsx
@@ -5,7 +5,7 @@ import { useAuth0} from "@auth0/auth0-react";
 function Navbar() {
   const navigate = useNavigate();
   const location = useLocation();
-  const {loginWithRedirect,logout, user, isLoading, isAuthenticated } = useAuth0();
+  const {loginWithRedirect, logout, user, isLoading, isAuthenticated } = useAuth0();
   return (
     <div className='navbar'>
       <div className='navbar-start'>
--- Client/src/components/profile.tsx
+++ Client/src/components/profile.tsx
@@ -1,9 +1,16 @@
 import '../css/profile.css';
+import { useAuth0, User} from "@auth0/auth0-react";
 
 function Profile() {
+  const { user } = useAuth0();
+
   return (
     <div>
-      <h1>Profile page</h1>
+      {
+        <h1>HELLO, {(user as User).name}</h1>
+      }
+      <h1>Your learning progress</h1>
+      <h1>Your games' stats</h1>
     </div>
   );
 }
