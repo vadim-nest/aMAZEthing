fab511a9aa5e731e83b5c6c8998352311783673a
Marija Vitkauskaite <m.v.vitkauskaite@gmail.com>
2022-11-27 14:55:44 +0000

    style: update bubble sort lesson design

--- Client/src/components/sortingLessons/bubble-lesson.tsx
+++ Client/src/components/sortingLessons/bubble-lesson.tsx
@@ -1,10 +1,10 @@
-import "../../css/sort-lesson.css";
-import { useEffect, useState } from "react";
-import bubbleSortAlgo from "../../utils/sorting-algo";
+import '../../css/sort-lesson.css';
+import { useState } from 'react';
+import bubbleSortAlgo from '../../utils/sorting-algo';
 
 export default function BubbleLesson() {
   const [array, setArray] = useState([5, 1, 4, 2, 8]);
-  const [clicked, setClicked] = useState(false)
+  const [clicked, setClicked] = useState(false);
   // Should store all the text/arrays in the db and just reuse this component for every lesson
   // But we'll just write everything here for now
   // Took the info from geeksforgeeks.com
@@ -12,17 +12,18 @@ export default function BubbleLesson() {
   const ARR_BAR_NUM = 8;
 
   let paragraphs = {
-    sortName: "Bubble sort",
+    sortName: 'Bubble sort',
     firstP:
-      "Bubble Sort is the simplest sorting algorithm that swaps two elements if they are in the wrong order. This algorithm is not suitable for large data sets as its average and worst-case time complexity is quite high.",
-    secondP: "As we go through each element, if the current element is bigger than the next one, we swap them"
+      'Bubble Sort is the simplest sorting algorithm that swaps two elements if they are in the wrong order. This algorithm is not suitable for large data sets as its average and worst-case time complexity is quite high.',
+    secondP:
+      'As we go through each element, if the current element is bigger than the next one, we swap them',
   };
 
   // Write a nice view for the columns in the array
 
   //----------------------------------------------------------
 
-  //random Array Gen func
+  // random Array Gen func
 
   // function genereateArray() {
   //   const array = []
@@ -41,65 +42,81 @@ export default function BubbleLesson() {
   // Animation to swap them
 
   async function bubbleSort() {
-    setClicked(true)
+    setClicked(true);
     const copyArr = array.slice();
-    const {ArrayStates, animations} = bubbleSortAlgo(copyArr);
-    console.log(animations)
-    for(let i = 0; i<animations.length; i++) {
-      await delay(500)
-      if(animations[i].length > 2) {
-        const [indexOne, elementOne, indexTwo, elementTwo] = animations[i]
-        document.getElementById(`${elementOne}`)!.style.backgroundColor = 'red'
-        document.getElementById(`${elementTwo}`)!.style.backgroundColor = 'red'
-        document.getElementById(`${elementOne}`)!.style.transform += `translateX(40px)`
-        document.getElementById(`${elementTwo}`)!.style.transform += `translateX(-40px)`
-        await delay(500)
-        document.getElementById(`${elementOne}`)!.style.backgroundColor = 'green'
-        document.getElementById(`${elementTwo}`)!.style.backgroundColor = 'green'
+    const { ArrayStates, animations } = bubbleSortAlgo(copyArr);
+    console.log(animations);
+    for (let i = 0; i < animations.length; i++) {
+      await delay(500);
+      if (animations[i].length > 2) {
+        const [indexOne, elementOne, indexTwo, elementTwo] = animations[i];
+        document.getElementById(`${elementOne}`)!.style.backgroundColor =
+          'var(--red)';
+        document.getElementById(`${elementTwo}`)!.style.backgroundColor =
+          'var(--red)';
+        document.getElementById(
+          `${elementOne}`
+        )!.style.transform += `translateX(40px)`;
+        document.getElementById(
+          `${elementTwo}`
+        )!.style.transform += `translateX(-40px)`;
+        await delay(500);
+        document.getElementById(`${elementOne}`)!.style.backgroundColor =
+          'var(--main-green)';
+        document.getElementById(`${elementTwo}`)!.style.backgroundColor =
+          'var(--main-green)';
       } else {
-        const [indexOne, elementOne] = animations[i]
-        document.getElementById(`${elementOne}`)!.style.backgroundColor = 'red'
-        await delay(500)
-        document.getElementById(`${elementOne}`)!.style.backgroundColor = 'green'
-
+        const [indexOne, elementOne] = animations[i];
+        document.getElementById(`${elementOne}`)!.style.backgroundColor =
+          'var(--red)';
+        await delay(500);
+        document.getElementById(`${elementOne}`)!.style.backgroundColor =
+          'var(--main-green)';
       }
     }
-      
   }
-  function delay(time:number) {
+  function delay(time: number) {
     return new Promise((res) => setTimeout(res, time));
   }
 
-
   return (
-      <div className="whole-page-wrapper">
-        <div className="lesson-wrapper">
-          <h1>{paragraphs.sortName}</h1>
-          <p>{paragraphs.firstP}</p>
-        </div>
+    <div className="whole-page-wrapper">
+      <div className="lesson-wrapper">
+        <h1>{paragraphs.sortName}</h1>
+        <p>{paragraphs.firstP}</p>
+      </div>
 
-        <div className="lesson-wrapper-2">
-          <p>{paragraphs.secondP}</p>
-            {!clicked && <button className="button clickSort" onClick={bubbleSort}> click </button>}
-        </div>
+      <div className="lesson-wrapper-2">
+        <p>{paragraphs.secondP}</p>
+      </div>
 
+      <div className="lesson-wrapper-3">
         <div className="array">
-            {array.map((element, index) => (
-              <div
+          {array.map((element, index) => (
+            <div
               className={`array-el`}
               style={{
-                backgroundColor: "green",
-                height: `${element*20}px`,
-                order : `${index}`
+                backgroundColor: 'var(--main-green)',
+                height: `${element * 20}px`,
+                order: `${index}`,
               }}
               id={`${element}`}
               key={index}
-              >
-                {" "}
-                {element}{" "}
-              </div>
-            ))}
-          </div>
+            >
+              {' '}
+              {element}{' '}
+            </div>
+          ))}
+        </div>
+        <div>
+          {!clicked && (
+            <button className="button clickSort" onClick={bubbleSort}>
+              {' '}
+              click{' '}
+            </button>
+          )}
+        </div>
       </div>
+    </div>
   );
 }
