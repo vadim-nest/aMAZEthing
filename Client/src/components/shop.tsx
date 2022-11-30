27a888819cb70bf750d9a64ef0e2abb2a2992cc8
Vadim Gnezdyshkin <vadim@gne.me.uk>
2022-11-30 17:06:40 +0000

    style: design the shop page

--- Client/src/components/shop.tsx
+++ Client/src/components/shop.tsx
@@ -14,22 +14,105 @@ export default function Shop({
       <h1>Shop</h1>
       <ul className='shop-list'>
         <div className='buy-minion-button' onClick={() => addNewMinion(squirrel)}>
-          <h3>Price, sortingalg, path finding, speed, </h3>
+          <div className='shop-stats'>
+            <h1 className='shop-just-stats'>Squirrel</h1>
+            <h1 className='shop-just-text'>Price</h1>
+            <h1 className='shop-just-stats price'>3000</h1>
+
+            <h1 className='shop-just-text'>Sorting algorithm</h1>
+            <h1 className='shop-just-stats'>{squirrel.sortingAlgo}</h1>
+
+            <h1 className='shop-just-text'>Path finding algorithm</h1>
+            <h1 className='shop-just-stats'>{squirrel.pathFindingAlgo}</h1>
+
+            <h1 className='shop-just-text'>Speed</h1>
+            <h1 className='shop-just-stats'>{squirrel.sortingSpeed}</h1>
+          </div>
           <Squirrel currentPlayer='p1-color' />
         </div>
         <div className='buy-minion-button' onClick={() => addNewMinion(badger)}>
+          <div className='shop-stats'>
+          <h1 className='shop-just-stats'>Badger</h1>
+            <h1 className='shop-just-text'>Price</h1>
+            <h1 className='shop-just-stats price'>3000</h1>
+
+            <h1 className='shop-just-text'>Sorting algorithm</h1>
+            <h1 className='shop-just-stats'>{badger.sortingAlgo}</h1>
+
+            <h1 className='shop-just-text'>Path finding algorithm</h1>
+            <h1 className='shop-just-stats'>{badger.pathFindingAlgo}</h1>
+
+            <h1 className='shop-just-text'>Speed</h1>
+            <h1 className='shop-just-stats'>{badger.sortingSpeed}</h1>
+          </div>
           <Badger currentPlayer='p1-color' />
         </div>
         <div className='buy-minion-button' onClick={() => addNewMinion(hare)}>
+          <div className='shop-stats'>
+          <h1 className='shop-just-stats'>Hare</h1>
+            <h1 className='shop-just-text'>Price</h1>
+            <h1 className='shop-just-stats price'>3000</h1>
+
+            <h1 className='shop-just-text'>Sorting algorithm</h1>
+            <h1 className='shop-just-stats'>{hare.sortingAlgo}</h1>
+
+            <h1 className='shop-just-text'>Path finding algorithm</h1>
+            <h1 className='shop-just-stats'>{hare.pathFindingAlgo}</h1>
+
+            <h1 className='shop-just-text'>Speed</h1>
+            <h1 className='shop-just-stats'>{hare.sortingSpeed}</h1>
+          </div>
           <Hare currentPlayer='p1-color' />
         </div>
         <div className='buy-minion-button' onClick={() => addNewMinion(deer)}>
+          <div className='shop-stats'>
+            <h1 className='shop-just-stats'>Deer</h1>
+            <h1 className='shop-just-text'>Price</h1>
+            <h1 className='shop-just-stats price'>3000</h1>
+
+            <h1 className='shop-just-text'>Sorting algorithm</h1>
+            <h1 className='shop-just-stats'>{deer.sortingAlgo}</h1>
+
+            <h1 className='shop-just-text'>Path finding algorithm</h1>
+            <h1 className='shop-just-stats'>{deer.pathFindingAlgo}</h1>
+
+            <h1 className='shop-just-text'>Speed</h1>
+            <h1 className='shop-just-stats'>{deer.sortingSpeed}</h1>
+          </div>
           <Deer currentPlayer='p1-color' />
         </div>
         <div className='buy-minion-button' onClick={() => addNewMinion(koala)}>
+          <div className='shop-stats'>
+            <h1 className='shop-just-stats'>Koala</h1>
+            <h1 className='shop-just-text'>Price</h1>
+            <h1 className='shop-just-stats price'>3000</h1>
+
+            <h1 className='shop-just-text'>Sorting algorithm</h1>
+            <h1 className='shop-just-stats'>{koala.sortingAlgo}</h1>
+
+            <h1 className='shop-just-text'>Path finding algorithm</h1>
+            <h1 className='shop-just-stats'>{koala.pathFindingAlgo}</h1>
+
+            <h1 className='shop-just-text'>Speed</h1>
+            <h1 className='shop-just-stats'>{koala.sortingSpeed}</h1>
+          </div>
           <Koala currentPlayer='p1-color' />
         </div>
         <div className='buy-minion-button' onClick={() => addNewMinion(bear)}>
+          <div className='shop-stats'>
+           <h1 className='shop-just-stats'>Bear</h1>
+            <h1 className='shop-just-text'>Price</h1>
+            <h1 className='shop-just-stats price'>3000</h1>
+
+            <h1 className='shop-just-text'>Sorting algorithm</h1>
+            <h1 className='shop-just-stats'>{bear.sortingAlgo}</h1>
+
+            <h1 className='shop-just-text'>Path finding algorithm</h1>
+            <h1 className='shop-just-stats'>{bear.pathFindingAlgo}</h1>
+
+            <h1 className='shop-just-text'>Speed</h1>
+            <h1 className='shop-just-stats'>{bear.sortingSpeed}</h1>
+          </div>
           <Bear currentPlayer='p1-color' />
         </div>
       </ul>
