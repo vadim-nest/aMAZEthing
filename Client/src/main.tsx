51a69db763dd98a32f94edf77151f883f4ff4326
Vadim Gnezdyshkin <vadim@gne.me.uk>
2022-12-01 19:56:12 +0000

    feat: start with the waiting room page

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -19,6 +19,7 @@ import DfsLesson from './components/pathfindingLessons/dfsLesson';
 import BfsLesson from './components/pathfindingLessons/bfsLesson';
 import DijkstraLesson from './components/pathfindingLessons/dijkstraLesson';
 import AllSortsPlay from './components/sortingLessons/allSortsPlay';
+import WaitingRoom from './components/waitingRoom';
 
 const router = createBrowserRouter([
   {
@@ -32,6 +33,10 @@ const router = createBrowserRouter([
       {
         path: '/callback',
         element: <Welcome />,
+      },
+      {
+        path: '/waitingRoom',
+        element: <WaitingRoom />,
       },
       {
         path: '/game',
