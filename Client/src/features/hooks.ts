0d7938a822e7d67433b7a0bd17a71fadde7e1930
Josep Abellana <josep.abe.pu@gmail.com>
2022-12-02 14:32:03 +0000

    feat(create algorithm hook): hook

--- Client/src/features/hooks.ts
+++ Client/src/features/hooks.ts
@@ -1,6 +1,28 @@
+import { useEffect,useState } from "react";
 import { TypedUseSelectorHook,useDispatch,useSelector } from "react-redux";
+import { bubbleSortAlgo } from "../utils/sorting-algo";
 import {RootState, AppDispatch} from './store'
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 //adding types to functions
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+
+
+
+
+
+
+function useAlgo(algoName:string = 'bubble', array: any = [], reversed:boolean = false) {
+    
+    const [animation,setAnimation] = useState([])
+    
+    useEffect(()=>{
+        switch(algoName){
+            case 'bubble':
+                setAnimation(bubbleSortAlgo(array,reversed) as any);
+        }
+    },[])
+  
+    return animation 
+  }
