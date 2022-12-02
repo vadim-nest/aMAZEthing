35d24e24119fc843686d2677aad38439c03080c0
Marija Vitkauskaite <m.v.vitkauskaite@gmail.com>
2022-12-02 12:03:12 +0000

    feat: create socket.io for create and join room in the waiting room

--- Client/src/components/waitingRoom.tsx
+++ Client/src/components/waitingRoom.tsx
@@ -1,15 +1,37 @@
 import '../css/waitingRoom.css';
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import socket from '../services/socket';
 
 function WaitingRoom() {
-
   const navigate = useNavigate();
   const [createClicked, setCreateClicked] = useState(false);
   const [joinClicked, setJoinClicked] = useState(false);
   const [playClicked, setPlayClicked] = useState(false);
+  const [id, setId] = useState('');
 
-  function onCreateCLicked () {
+  useEffect(() => {
+    socket.on('Game start', () => {
+      console.log('Game started');
+    });
+  }, []);
+
+  function hostRoom() {
+    socket.emit('host', socket.id);
+    console.log('socket.id', socket.id);
+    setId(socket.id);
+  }
+
+  function joinRoom(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const target = e.target as typeof e.target & {
+      fname: { value: string };
+    };
+    const room = target.fname.value;
+    socket.emit('join', room);
+  }
+
+  function onCreateCLicked() {
     if (!createClicked) {
       // greyOutOtherButtons('Create');
       setCreateClicked(true);
@@ -20,7 +42,7 @@ function WaitingRoom() {
     }
   }
 
-  function onJoinCLicked () {
+  function onJoinCLicked() {
     if (!joinClicked) {
       setJoinClicked(true);
       setCreateClicked(false);
@@ -30,56 +52,102 @@ function WaitingRoom() {
     }
   }
 
-  function onPlayClicked () {
+  function onPlayClicked() {
     if (!playClicked) {
       setPlayClicked(true);
       setJoinClicked(false);
       setCreateClicked(false);
       // navigate('/game');
-      setTimeout(() => navigate('/game'), 3000)
+      setTimeout(() => navigate('/game'), 3000);
     } else {
       setPlayClicked(false);
     }
   }
 
-
   return (
-    <div className='waiting-room'>
-
-      <h3 className='explanation-text'><span className='wait-r-yellow'>Create</span> a private party or <span className='wait-r-red'>search</span> for a game</h3>
-      <div className='selection-panel'>
-        <div className='left-side-selection'>
-          <button className='wr-main-button'><h1 className={`waiting-page-create-button ${createClicked && 'wait-r-yellow'} ${playClicked && 'playClicked'} ${joinClicked && 'joinClicked'}`} onClick={() => onCreateCLicked()}>Create</h1></button>
-          {createClicked
-            ? (<input className='create-room-code' type="text" id="fname" name="fname" placeholder='fgd83'/>)
-            : <button className='wr-main-button'><h1 className={`waiting-page-join-button ${joinClicked && 'wait-r-yellow'} ${playClicked && 'playClicked'} ${createClicked && 'createClicked'}`} onClick={() => onJoinCLicked()}>Join</h1></button>}
-        {joinClicked
-        ? (<>
-            <div className='join-code-input'>
-              <input className='create-room-code join-room-code' type="text" id="fname" name="fname" placeholder='type the code here'/>
-              <button className='submit-the-code-button'>✓</button>
-            </div>
-          </>
-        )
-        : <></>}
+    <div className="waiting-room">
+      <h3 className="explanation-text">
+        <span className="wait-r-yellow">Create</span> a private party or{' '}
+        <span className="wait-r-red">search</span> for a game
+      </h3>
+      <div className="selection-panel">
+        <div className="left-side-selection">
+          <form onSubmit={joinRoom}>
+            <button className="wr-main-button" type="button" onClick={hostRoom}>
+              <h1
+                className={`waiting-page-create-button ${
+                  createClicked && 'wait-r-yellow'
+                } ${playClicked && 'playClicked'} ${
+                  joinClicked && 'joinClicked'
+                }`}
+                onClick={() => onCreateCLicked()}
+              >
+                Create
+              </h1>
+            </button>
+            {createClicked ? (
+              <p>
+                {id}
+              </p>
+            ) : (
+              <button className="wr-main-button">
+                <h1
+                  className={`waiting-page-join-button ${
+                    joinClicked && 'wait-r-yellow'
+                  } ${playClicked && 'playClicked'} ${
+                    createClicked && 'createClicked'
+                  }`}
+                  onClick={() => onJoinCLicked()}
+                >
+                  Join
+                </h1>
+              </button>
+            )}
+            {joinClicked && (
+              <>
+                <div className="join-code-input">
+                  <input
+                    className="create-room-code join-room-code"
+                    type="text"
+                    id="fname"
+                    name="fname"
+                    placeholder="type the code here"
+                    required
+                  />
+                  <button className="submit-the-code-button">✓</button>
+                </div>
+              </>
+            )}
+          </form>
         </div>
-        <div className='right-side-selection'>
-          <button className='wr-main-button' onClick={() => onPlayClicked()}><h1 className={`wr-play-button ${playClicked && 'wait-r-red'} ${joinClicked && 'joinClicked'} ${createClicked && 'createClicked'}`}>Play</h1></button>
-          { playClicked
-            ? ( <>
-                  <h3 className='play-h3 wr-amazing-text'>Looking for <span className='wait-r-red'>play</span>ers</h3>
-                  <div className='loading-animation'/>
-                </>
-            )
-            : (<div className='wr-amazing-text'>
-                <p>The most amazing</p>
-                <p>learning game.</p>
-              </div>
-              )}
+
+        <div className="right-side-selection">
+          <button className="wr-main-button" onClick={() => onPlayClicked()}>
+            <h1
+              className={`wr-play-button ${playClicked && 'wait-r-red'} ${
+                joinClicked && 'joinClicked'
+              } ${createClicked && 'createClicked'}`}
+            >
+              Play
+            </h1>
+          </button>
+          {playClicked ? (
+            <>
+              <h3 className="play-h3 wr-amazing-text">
+                Looking for <span className="wait-r-red">play</span>ers
+              </h3>
+              <div className="loading-animation" />
+            </>
+          ) : (
+            <div className="wr-amazing-text">
+              <p>The most amazing</p>
+              <p>learning game.</p>
+            </div>
+          )}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default WaitingRoom;
