aae6d5885807aae66a5590f9cec39b4c3396b157
Josep Abellana <josep.abe.pu@gmail.com>
2022-12-02 12:58:04 +0000

    feat(changes on profile): profile changes

--- Client/src/components/welcome.tsx
+++ Client/src/components/welcome.tsx
@@ -1,9 +1,6 @@
 import '../css/welcome.css';
 import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
-import { useAuth0 } from "@auth0/auth0-react";
 import MediaQuery from 'react-responsive';
-import apiService from '../services/apiService';
 
 function Welcome() {
 
@@ -13,31 +10,19 @@ function Welcome() {
     <div className='welcome-page'>
       <MediaQuery minWidth={951}>
         <h1 className='welcome'>WELCOME.</h1>
-<<<<<<< HEAD
         <button className='options' onClick={() => navigate('/waitingRoom')}>PLAY</button>
-=======
-        <button id="play-button" className='options' onClick={() => navigate('/game')}>PLAY</button>
->>>>>>> 35d24e24119fc843686d2677aad38439c03080c0
         <br/>
         <button className='options' onClick={() => navigate('/learning')}>LEARN</button>
       </MediaQuery>
       <MediaQuery maxWidth={951} minWidth={651}>
         <h1 className='welcome-medium'>WELCOME.</h1>
-<<<<<<< HEAD
         <button className='options-medium' onClick={() => navigate('/waitingRoom')}>PLAY</button>
-=======
-        <button id="play-button" className='options-medium' onClick={() => navigate('/game')}>PLAY</button>
->>>>>>> 35d24e24119fc843686d2677aad38439c03080c0
         <br/>
         <button className='options-medium' onClick={() => navigate('/learning')}>LEARN</button>
       </MediaQuery>
       <MediaQuery maxWidth={650}>
         <h1 className='welcome-small'>WEL</h1><h1 className='welcome-small'>COME.</h1>
-<<<<<<< HEAD
         <button className='options-small' onClick={() => navigate('/waitingRoom')}>PLAY</button>
-=======
-        <button id="play-button" className='options-small' onClick={() => navigate('/game')}>PLAY</button>
->>>>>>> 35d24e24119fc843686d2677aad38439c03080c0
         <br/>
         <button className='options-small' onClick={() => navigate('/learning')}>LEARN</button>
       </MediaQuery>
--- server/cypress/e2e/Auth0/testMaze.cy.ts
+++ server/cypress/e2e/Auth0/testMaze.cy.ts
@@ -2,14 +2,18 @@
 
 context('Maze Tests', () => {
     beforeEach(() => {
-      cy.visit('http://localhost:5173');
-      cy.wait(500);
-      cy.get('#play-button').click();
+        cy.visit('http://localhost:5173/game', { timeout: 3000 });
         cy.wait(4000);
     })
     it('Place a minion, order it to move', () => {
-        
 
+        cy.get('#store-button').click();
+        cy.wait(400);
+        cy.get('#first-minion-buy').click();
+        cy.wait(400);
+        cy.get('#1136').rightclick();
+        cy.wait(30000);
+        cy.get('#text-reverse-nope').should('should.have','1');
     });
 
 });
