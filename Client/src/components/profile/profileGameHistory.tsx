48e8c36b2aa58885897bed09bbafc6439c9e27f8
Marija Vitkauskaite <m.v.vitkauskaite@gmail.com>
2022-12-06 14:20:57 +0000

    feat: change color in profile based on win/loss/draw

--- Client/src/components/profile/profileGameHistory.tsx
+++ Client/src/components/profile/profileGameHistory.tsx
@@ -7,38 +7,59 @@ import CoinSVG from './coin';
 export default function ProfileGameHistory() {
   const user = useAppSelector((state) => state.user);
   //Im hard coding the data
-    const games = user.games;
+  const games = user.games;
 
   return (
     <div className="gameDashboard">
       {games.map((element: any, index: number) => {
         console.log(element);
         return (
-          <div className="match" key={index}>
+          <div
+            className={` match
+          ${element.result === 'win' && 'match-win'}
+          ${element.result === 'loss' && 'match-loss'}
+          ${element.result === 'draw' && 'match-draw'}
+          `}
+            key={index}
+          >
             <div className="gameDuration">
               <div>
-                <h1>{element.winner}</h1>
-                {element.result === 'win' ? <h1 className="result-win">{element.result}</h1> : <h1 className="result-loss">{element.result}</h1>}
+                {element.result === 'win' && (
+                  <h1 className="result-win">{element.result}</h1>
+                )}
+                {element.result === 'loss' && (
+                  <h1 className="result-loss">{element.result}</h1>
+                )}
+                {element.result === 'draw' && (
+                  <h1 className="result-draw">{element.result}</h1>
+                )}
               </div>
-              <div className="duration"><h3 className='durationDate'>VS: {element.opponentUsername}</h3></div>
-              <div className="duration"><h3 className='durationDate'>TIME: 5 min</h3></div>
-              <div className="date"><h3 className='durationDate'>DATE: {new Date(element.finishedAt).toLocaleString()}</h3></div>
+              <div className="duration">
+                <h3 className="durationDate">VS: {element.opponentUsername}</h3>
+              </div>
+              <div className="date">
+                <h3 className="durationDate">
+                  DATE: {new Date(element.finishedAt).toLocaleString()}
+                </h3>
+              </div>
             </div>
             <div className="minionStats">
-              <div className='image-profile'>
-                <Squirrel currentPlayer="p1-color"/>
+              <div className="image-profile">
+                <Squirrel currentPlayer="p1-color" />
               </div>
-                <h3 className='gameStatsText'>{element.minions} Minions</h3>
+              <h3 className="gameStatsText">{element.minions} Minions</h3>
             </div>
             <div className="towerStats">
-                <div className='image-profile'>
-                  <TowerSVG playerClass='neutralTower'/>
-                </div>
-                <h3 className='gameStatsText'>{element.towers} Towers</h3>
+              <div className="image-profile">
+                <TowerSVG playerClass="neutralTower" />
+              </div>
+              <h3 className="gameStatsText">{element.towers} Towers</h3>
             </div>
             <div className="goldStats">
-                <div className='image-profile'><CoinSVG/></div>
-                <h3 className='gameStatsText'>{element.gold} Gold</h3>
+              <div className="image-profile">
+                <CoinSVG />
+              </div>
+              <h3 className="gameStatsText">{element.gold} Gold</h3>
             </div>
           </div>
         );
