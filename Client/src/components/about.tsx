9b9783de66e7161ab5e896191bcc19ad59d75707
Vadim Gnezdyshkin <vadim@gne.me.uk>
2022-12-07 16:01:43 +0000

    feat: start with the about page

--- Client/src/components/about.tsx
+++ Client/src/components/about.tsx
@@ -1,9 +1,23 @@
 import '../css/about.css';
+import Josep from '../assets/authors-pics/josep.jpg'
+import Marija from '../assets/authors-pics/marija.jpg'
+import Jacky from '../assets/authors-pics/jacky.jpg'
+import Issac from '../assets/authors-pics/isaac.jpg'
+import Vadim from '../assets/authors-pics/vadim.jpg'
 
 function About() {
   return (
     <div>
-      <h1>About page</h1>
+      <h3>Our goal was to provide easy-to-understand learning experience that will help you to finally comprehend sorting and path finding algorithms.</h3>
+      <h3>We tried to use all of the different algorithms in the game we created, so that it is more exciting for the user.</h3>
+
+      <div className='authors'>
+        <a href='https://github.com/josepabellana' target="_blank"><img src={Josep}/></a>
+        <a href='https://github.com/MarijaVitkauskaite' target="_blank"><img src={Marija}/></a>
+        <a href='https://github.com/majilaii' target="_blank"><img src={Jacky}/></a>
+        <a href='https://github.com/IP1llar' target="_blank"><img src={Issac}/></a>
+        <a href='https://github.com/vadim-nest' target="_blank"><img src={Vadim}/></a>
+      </div>
     </div>
   );
 }
