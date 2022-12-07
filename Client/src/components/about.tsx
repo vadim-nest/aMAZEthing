29b1d12d9857b3e612bd127069075cc8a343ae0e
Vadim Gnezdyshkin <vadim@gne.me.uk>
2022-12-07 17:46:46 +0000

    feat: about page done

--- Client/src/components/about.tsx
+++ Client/src/components/about.tsx
@@ -7,9 +7,11 @@ import Vadim from '../assets/authors-pics/vadim.jpg'
 
 function About() {
   return (
-    <div>
-      <h3>Our goal was to provide easy-to-understand learning experience that will help you to finally comprehend sorting and path finding algorithms.</h3>
-      <h3>We tried to use all of the different algorithms in the game we created, so that it is more exciting for the user.</h3>
+    <div className='about-wrapper'>
+      <div className='about-text'>
+        <h3>Our goal was to provide easy-to-understand learning experience that will help you to finally comprehend sorting and path finding algorithms.</h3>
+        <h3>We tried to use all of the different algorithms in the game we created, so that it is more exciting for the user.</h3>
+      </div>
 
       <div className='authors'>
         <a href='https://github.com/josepabellana' target="_blank"><img src={Josep}/></a>
